64ff5d2f78ccf62a46d1134b9c5c6ff434147f38
fabricioluna <fabricioluna@gmail.com>
2025-06-03 20:52:29 -0300

    Add files via upload

---
+++ api/times.js
@@ -0,0 +1,24 @@
+
+export default async function handler(req, res) {
+  const ids = [295114, 22845712, 1667349, 431514, 272336, 2827906, 18767307, 44845957, 44730884, 47668576, 50359784, 47794095, 1294522, 9441345, 26086353, 10392741, 15915, 299125, 8842639, 19191394, 207131, 2238134, 1646843, 254144, 340238, 8295951, 49122213, 271219, 396913, 390492, 6762375, 47841417, 20953551, 6877095, 1002672, 5249257, 19415623, 1567740, 29174951, 28912126];
+  const resultados = [];
+
+  for (const id of ids) {
+    try {
+      const response = await fetch(`https://api.cartola.globo.com/time/id/${id}`);
+      const data = await response.json();
+      resultados.push({
+        id: data.time_id,
+        nome: data.nome,
+        nome_cartola: data.nome_cartola,
+        escudo_url: data.escudo_url,
+        pontos: data.pontos,
+        patrimonio: data.patrimonio
+      });
+    } catch (e) {
+      console.error("Erro ao buscar dados do ID", id, e);
+    }
+  }
+
+  res.status(200).json(resultados);
+}
