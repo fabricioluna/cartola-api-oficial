add26f8e6988f96e663dd98e9320ff76140ae2a2
fabricioluna <fabricioluna@gmail.com>
2025-06-03 21:44:52 -0300

    escudo.js

--- api/escudo.js
+++ api/escudo.js
@@ -1,15 +1,16 @@
-
 import got from 'got';
+import { URL } from 'url';
 
 export default async function handler(req, res) {
-  const { url } = req.query;
+  const reqUrl = new URL(req.url, `http://${req.headers.host}`);
+  const escudoUrl = reqUrl.searchParams.get('url');
 
-  if (!url || !url.startsWith("https://")) {
+  if (!escudoUrl || !escudoUrl.startsWith("https://")) {
     return res.status(400).json({ error: "URL inválida" });
   }
 
   try {
-    const stream = got.stream(url);
+    const stream = got.stream(escudoUrl);
     stream.on('response', (response) => {
       res.setHeader("Content-Type", response.headers['content-type']);
       res.setHeader("Access-Control-Allow-Origin", "*");
